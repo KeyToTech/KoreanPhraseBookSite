063362b1fc3c4649a61fe4c4a1155bf15ac62a06
Diana Brychuk <dianabrychuk@gmail.com>
2018-10-17 15:51:11 +0300

    add header image on header block

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -11,49 +11,51 @@ class Header extends Component {
         return (
           
 <div className="header">
-<div className="left_header_block">
-  <div className="left_top_block">
-  <div className="paralelogram">
-  </div>
-  <div className="triangle">
-  </div>
-</div>
+    <div className='header-block'>
+        <div className="left_header_block">
+          <div className="left_top_block">
+          <div className="paralelogram">
+          </div>
+          <div className="triangle">
+          </div>
+        </div>
 
-<div className="button_paralelogram">
-</div>
+        <div className="button_paralelogram">
+        </div>
 
 
-  <div className="average_header_block">
-      <div className="logo_header">
-        <h1>Korean Phrases</h1>
+          <div className="average_header_block">
+              <div className="logo_header">
+                <h1>Korean Phrases</h1>
+              </div>
+
+            <div className="info_header_block">
+              <div className="info_block">
+                <h2>The Korean Phrases.....</h2>
+                <h3>Every time you sign up <br/> a friend to VITAL you earn 4 referral points. <br/>You also get 2 referral points for people your....</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+          <div className="right_header_block">
+            <div className="header_button">
+              <div className="button_android">
+                <img className="android_img" src={googlePlayBadge}/>
+              </div>
+              <div className="button_ios">
+                <img className="ios_img" src={iosBadge}/>
+              </div>
+            </div>
+
+          </div>
       </div>
 
-    <div className="info_header_block">
-      <div className="info_block">
-        <h2>The Korean Phrases.....</h2>
-        <h3>Every time you sign up <br/> a friend to VITAL you earn 4 referral points. <br/>You also get 2 referral points for people your....</h3>
-      </div>
-    </div>
-  </div>
-</div>
-
-  <div className="right_header_block">
-    <div className="header_button">
-      <div className="button_android">
-        <img className="android_img" src={googlePlayBadge}/>
-      </div>
-      <div className="button_ios">
-        <img className="ios_img" src={iosBadge}/>
-      </div>
-    </div>
-
-    <div className="image_header_block">
-      <div className="header_image">
-        <img className='app-illustration' src={ koreanImg } />
-      </div>
-    </div>
-
-  </div>
+        <div className="image_header_block">
+                <div className="header_image">
+                  <img className='app-illustration' src={ koreanImg } />
+                </div>
+              </div>
 
 </div>
         );
