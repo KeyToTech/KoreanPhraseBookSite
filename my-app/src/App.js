1c4360399dc37d03864adf461e4ccd2ad5b398ad
OlehBabiy <luisowen11@gmail.com>
2018-10-18 14:04:59 +0300

    finished scaling for header)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './components/header.js';
 import Content from './components/content.js';
 import Footer from './components/footer.js';
 import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (
-     <div>
-       <Header/>
-        <Content/>
-        <Footer/>
-     </div>
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
 
-    );
-  }
+        );
+    }
 }
 
 export default App;
