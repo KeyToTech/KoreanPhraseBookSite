894f7d28f234b937c6cd9bc39724ef003ae6ee50
OlehBabiy <luisowen11@gmail.com>
2018-10-18 17:50:42 +0300

    finished scaling)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
             <div>
                 <Header/>
                 <Content/>
+                <Footer/>
             </div>
 
         );
--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,66 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import appLogo from '../res/logo.png';
 import icFacebook from '../res/ic-facebook.png';
 import icInstagram from '../res/ic-instagram.png';
 import icTwitter from '../res/ic-twitter.png';
 import './footer.css';
 
-class Footer extends Component{
-    render(){
-        return(
+class Footer extends Component {
+    render() {
+        return (
             <footer>
-            <div className="left_block">
-              <div className="left_footer_block">
-                <div className="logo_footer">
-                  <img className="logo_img" src={ appLogo }/>
-                  <h1>Korean phrases</h1>
+                <div className="left_block">
+                    <div className="left_footer_block">
+                        <div className="logo_footer">
+                            <img className="logo_img" src={appLogo}/>
+                            <h1>Korean phrases</h1>
+                        </div>
+                        <div className="info_block">
+                            <h3>Every time you sign up <br/> a friend to VITAL you earn 4 referral points. <br/> You
+                                also get 2 referral points for people your....</h3>
+                        </div>
+                        <div className="contact_block">
+                            <h3>Copyright © 2018 KeyToTech | All Rights Reserved</h3>
+                        </div>
+                    </div>
                 </div>
-                <div className="info_block">
-                  <h3>Every time you sign up <br/> a friend to VITAL you earn 4 referral points.  <br/> You also get 2 referral points for people your....</h3>
+
+
+                <div className="menu_footer_block">
+                    <div className="menu_items">
+                        <div className="menu_item">
+                            <h2>About</h2>
+                        </div>
+                        <div className="menu_item">
+                            <h2>Price</h2>
+                        </div>
+                        <div className="menu_item">
+                            <h2>Features</h2>
+                        </div>
+                    </div>
                 </div>
-                <div className="contact_block">
-                  <h3>Copyright © 2018 KeyToTech | All Rights Reserved</h3>
+
+
+                <div className="right_block">
+                    <div className="follow_block">
+                        <div className="follow_us">
+                            <h2>Follow</h2>
+                        </div>
+                        <div className="follow_items">
+                            <div className="follow_item">
+                                <img className="foolow_img" src={icFacebook}/>
+                            </div>
+                            <div className="follow_item">
+                                <img className="foolow_img" src={icTwitter}/>
+                            </div>
+                            <div className="follow_item">
+                                <img className="foolow_img" src={icInstagram}/>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-              </div>
-            </div>
-        
-        
-            <div className="menu_footer_block">
-              <div className="menu_items">
-                <div className="menu_item">
-                  <h2>About</h2>
-                </div>
-                <div className="menu_item">
-                  <h2>Price</h2>
-                </div>
-                <div className="menu_item">
-                  <h2>Features</h2>
-                </div>
-              </div>
-            </div>
-        
-        
-        
-        <div className="right_block">
-          <div className="follow_block">
-            <div className="follow_us">
-              <h2>Follow</h2>
-            </div>
-            <div className="follow_items">
-              <div className="follow_item">
-              <img className="foolow_img" src={icFacebook}/>
-            </div>
-              <div className="follow_item">
-                <img className="foolow_img" src={icTwitter}/>
-              </div>
-                <div className="follow_item">
-                  <img className="foolow_img" src={icInstagram}/>
-            </div>
-          </div>
-          </div>
-        
-        </div>
-          </footer>
+            </footer>
         );
     }
 }
