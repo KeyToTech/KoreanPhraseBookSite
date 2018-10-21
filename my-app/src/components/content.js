c8a362b02a5ba01ca6bf856df3cb8b6cdb7b52af
Bogdan Ustyak <bogdan.ustyak@gmail.com>
2018-10-21 17:47:52 +0300

    added android screenshots,  refactored list of features, extracted code duplications into a functions.

--- my-app/src/components/content.js
+++ my-app/src/components/content.js
@@ -1,53 +1,56 @@
 import React, {Component} from 'react';
-import stubImage from '../res/stub_image.jpg';
+import chaptersAndroid from '../res/screenshots/android/rozdil_framed.png';
+import dictAndroid from '../res/screenshots/android/dict_framed.png';
+import favAndroid from '../res/screenshots/android/favourite_framed.png';
+import dialogsAndroid from '../res/screenshots/android/dialogs_android.png';
+import AppInfo from "./appInfo";
 import './content.css'
+
+const INFO = [
+    {
+      textFirst: true, 
+      detailsInfo: {
+        title: 'Розділи',
+        description: 'Знайомство, покупка одягу і навіть оренда авто. Тематично поділені слова та вирази допоможуть вам легко висловити свою думку та знайти спільну мову з оточуючими.'
+      },
+      androidImage: chaptersAndroid
+    },
+    {
+        textFirst: false, 
+        detailsInfo: {
+            title: 'Cловник',
+            description: 'Словник містить понад 900 слів. Ми зробили пошук по алфавіту, щоб ви могли миттєво переходити від А до Я.'
+        },
+        androidImage: dictAndroid
+      },
+      {
+        textFirst: true, 
+        detailsInfo: {
+            title: 'Улюблене',
+            description: 'Створіть власний набір фраз, зберігаючи їх в улюбленому.'
+        },
+        androidImage: favAndroid
+      },
+      {
+        textFirst: false, 
+        detailsInfo: {
+            title: 'Діалоги',
+            description: 'Різні життєві ситуації, представлені у вигляді діалогів між двома співрозмовниками. Це чудовий спосіб практикування мови!'
+        },
+        androidImage: dialogsAndroid
+      }
+  ];
 
 class Content extends Component {
     render() {
         return (
             <div className="content">
                 <div className="wrapp_info_section">
-                    <div className="app_info_section">
-                        <div className="text">
-                            <h1 className="info_text">Розділи</h1>
-                            <p className='description'>
-                                Знайомство, покупка одягу і навіть оренда авто. Тематично поділені слова та вирази
-                                допоможуть вам легко висловити свою думку та знайти спільну мову з оточуючими. </p>
-                        </div>
-                        <div className="screen">
-                            <img src={stubImage}/>
-                        </div>
-                    </div>
-                    <div className="app_info_section">
-                        <div className="screen">
-                            <img src={stubImage}/>
-                        </div>
-                        <div className="text_left">
-                            <h1 className="info_text">Cловник</h1>
-                            <p className='description'>Словник містить понад 900 слів. Ми зробили пошук по алфавіту, щоб ви могли миттєво
-                                переходити від А до Я.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="app_info_section">
-                        <div className="text">
-                            <h1 className="info_text">Улюблене</h1>
-                            <p className='description'>Створіть власний набір фраз, зберігаючи їх в улюбленому. </p>
-                        </div>
-                        <div className="screen">
-                            <img src={stubImage}/>
-                        </div>
-                    </div>
-                    <div className="app_info_section">
-                        <div className="screen">
-                            <img src={stubImage}/>
-                        </div>
-                        <div className="text_left">
-                            <h1 className="info_text">Діалоги</h1>
-                            <p className='description'>Різні життєві ситуації, представлені у вигляді діалогів між двома співрозмовниками. Це
-                                чудовий спосіб практикування мови!</p>
-                        </div>
-                    </div>
+                    {
+                        INFO.map((infoItem, i) => {
+                            return(<AppInfo info= { infoItem }/>)
+                        })
+                    }                    
                 </div>
             </div>
         );
