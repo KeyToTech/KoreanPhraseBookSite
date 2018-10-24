7065041d3734b4024294a4b346f0fd2a1c02b7d8
OlehBabiy <luisowen11@gmail.com>
2018-10-24 16:25:52 +0300

    finished refactoring code content css)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends React.Component {
         return (
             <div>                
                 <Header/>
+                <Content/>
+                <Footer/>
             </div>
 
         );
