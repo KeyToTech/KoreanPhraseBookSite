b85768ae99cc077cb0dc98f924333c831d66811b
OlehBabiy <luisowen11@gmail.com>
2018-12-12 14:52:50 +0200

    changed the structure of the HTML code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,29 +15,26 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className='header-block'>
-          <div className="left_header_block">
-            <div className="left_top_block">
-              <div className="paralelogram">
-              </div>
-              <div className="triangle">
-              </div>
+        <div className="figure-block">
+          <div className="top-figure">
+            <div className="paralelogram">
             </div>
-
-            <div className="button_paralelogram">
-            </div>
-
-
-            <div className="average_header_block">
-
-              <div className="info_header_block">
-                <h2 className='title'>{title}</h2>
-                <h3 className='subtitle'>{subtitle}</h3>
-              </div>
+            <div className="triangle">
             </div>
           </div>
+          <div className="bottom-figure">
+            <div className="bottom-paralelogram"></div>
+          </div>
+        </div>
 
-          <div className="right_header_block">
+        <div className="main-block">
+          <div className="content-header">
+            <div className="info-header">
+              <h2 className='title'>{title}</h2>
+              <h3 className='subtitle'>{subtitle}</h3>
+            </div>
+          </div>
+          <div className="social-networks-block">
             <div className="button_wrap">
               <div className="button_android">
                 <a href="https://play.google.com/store/apps/details?id=com.keytotech.koreanphrasebook" class="active">
@@ -50,19 +47,68 @@ class Header extends Component {
                 </a>
               </div>
             </div>
-
           </div>
         </div>
 
-        <div className="image_header_block">
-          <div className="header_image">
-            <img className='app-illustration' src={koreanImg} />
-          </div>
+        <div className="image-header">
+          <img className='app-illustration' src={koreanImg} />
         </div>
 
       </div>
     );
   }
 }
+
+
+// top-figuare
+
+{/* <d  iv className="left_top_block">
+<div className="paralelogram">
+</div>
+<div className="triangle">
+</div>
+</div> */}
+
+// bottom-figure
+
+{/* <div className="button_paralelogram"></div> */ }
+
+
+
+// content-header
+
+{/* <div className="average_header_block">
+
+  <div className="info_header_block">
+    <h2 className='title'>{title}</h2>
+    <h3 className='subtitle'>{subtitle}</h3>
+  </div>
+</div> */}
+
+
+// social-networks-block
+
+{/* <div className="right_header_block">
+  <div className="button_wrap">
+    <div className="button_android">
+      <a href="https://play.google.com/store/apps/details?id=com.keytotech.koreanphrasebook" class="active">
+        <img className="android_img" src={googlePlayBottom} />
+      </a>
+    </div>
+    <div className="button_ios">
+      <a href="http://www.keytotech.com/" class="active">
+        <img className="ios_img" src={iosStoreBottom} />
+      </a>
+    </div>
+  </div>
+</div> */}
+
+// image-header
+
+{/* <div className="image_header_block">
+  <div className="header_image">
+    <img className='app-illustration' src={koreanImg} />
+  </div>
+</div> */}
 
 export default Header;
