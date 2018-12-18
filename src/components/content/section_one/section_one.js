232258362e00065d2501f5c031a58b4d24ebcfbe
Babiy7 <luisowen11@gmail.com>
2018-12-18 17:35:38 +0200

    add material design

--- src/components/content/section_one/section_one.js
+++ src/components/content/section_one/section_one.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../../content/style.css'
+import '../../content/style.css';
+import Button from '@material-ui/core/Button';
 
 
 class SectionOne extends Component {
@@ -10,15 +11,48 @@ class SectionOne extends Component {
                 <div className="row">
 
                     <div className="collumn-green">
+                        <div className="image">
                         <div className="image"></div>
-                        <div className="content"></div>
+                        </div>
+                        <div className="content-text">
+                        <h1 className="title">Email Marketing</h1>
+                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
                         <div className="button">
-                           
+                            <Button variant="contained" color="primary" >
+                                Learn More
+                           </Button>
                         </div>
                     </div>
 
-                    <div className="collumn-blue"></div>
-                    <div className="collumn-red"></div>
+                    <div className="collumn-blue">
+                    <div className="image">
+                        <div className="image"></div>
+                        </div>
+                        <div className="content-text">
+                        <h1 className="title">Email Marketing</h1>
+                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div className="button">
+                            <Button variant="contained" color="primary" >
+                                Learn More
+                           </Button>
+                        </div>
+                    </div>
+                    <div className="collumn-red">
+                    <div className="image">
+                        <div className="image"></div>
+                        </div>
+                        <div className="content-text">
+                        <h1 className="title">Email Marketing</h1>
+                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        <div className="button">
+                            <Button variant="contained" color="primary" >
+                                Learn More
+                           </Button>
+                        </div>
+                    </div>
 
 
                 </div>
