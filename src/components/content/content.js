470d5707678e44dbe47bb859bab60af28bf8bd00
Babiy7 <luisowen11@gmail.com>
2018-12-19 17:05:52 +0200

    section one done

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -5,6 +5,7 @@ import favAndroid from '../../res/screenshots/android/favourite_framed.png';
 import dialogsAndroid from '../../res/screenshots/android/dialogs_android.png';
 import '../content/content.css'
 import SectionOne from '../content/section_one/section_one.js';
+import SectionTwo from '../content/section_two/section_two.js';
 
 
 class Content extends Component {
@@ -13,6 +14,7 @@ class Content extends Component {
             <div className="content">
                 <div className="wrapp_info_section">
                    <SectionOne/>
+                   <SectionTwo/>
                 </div>
             </div>
             );
--- src/components/content/section_one/section_one.js
+++ src/components/content/section_one/section_one.js
@@ -1,63 +1,58 @@
 import React, { Component } from 'react';
 import '../../content/style.css';
-import Button from '@material-ui/core/Button';
+
 
 
 class SectionOne extends Component {
     render() {
         return (
-            <section className="main-container">
+            <section className="wrapp_info_section_one">
 
-                <div className="row">
+                <div className="main-container">
+                    <div className="row">
 
-                    <div className="collumn-green">
-                        <div className="image">
-                        <div className="image"></div>
+                        <div className="collumn-green">
+                            <div className="image">
+
+                            </div>
+                            <div className="content-text">
+                                <h1 className="title">Email Marketing</h1>
+                                <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div className="button">
+                                <a href="#">Learn More</a>
+                            </div>
                         </div>
-                        <div className="content-text">
-                        <h1 className="title">Email Marketing</h1>
-                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <div className="collumn-blue">
+                            <div className="image">
+
+                            </div>
+                            <div className="content-text">
+                                <h1 className="title">Email Marketing</h1>
+                                <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div className="button">
+                                <a href="#">Learn More</a>
+                            </div>
                         </div>
-                        <div className="button">
-                            <Button variant="contained" color="primary" >
-                                Learn More
-                           </Button>
+                        <div className="collumn-red">
+                            <div className="image">
+
+                            </div>
+                            <div className="content-text">
+                                <h1 className="title">Email Marketing</h1>
+                                <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div className="button-container">
+                                <a href="#" className="button">Learn More</a>
+                            </div>
                         </div>
+
+
                     </div>
-
-                    <div className="collumn-blue">
-                    <div className="image">
-                        <div className="image"></div>
-                        </div>
-                        <div className="content-text">
-                        <h1 className="title">Email Marketing</h1>
-                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                        <div className="button">
-                            <Button variant="contained" color="primary" >
-                                Learn More
-                           </Button>
-                        </div>
-                    </div>
-                    <div className="collumn-red">
-                    <div className="image">
-                        <div className="image"></div>
-                        </div>
-                        <div className="content-text">
-                        <h1 className="title">Email Marketing</h1>
-                        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        </div>
-                        <div className="button">
-                            <Button variant="contained" color="primary" >
-                                Learn More
-                           </Button>
-                        </div>
-                    </div>
-
 
                 </div>
-
-
 
             </section>
         );
