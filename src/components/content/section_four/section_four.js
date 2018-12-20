18da111a2d4e661ce17a9937c13938f0251d4871
Babiy7 <luisowen11@gmail.com>
2018-12-20 17:24:41 +0200

    Content: create 1) section_four.js

---
+++ src/components/content/section_four/section_four.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import '../../content/style_four.css';
+
+
+class SectionFour extends Component {
+    render() {
+        return (
+            <section className="wrapp_info_section_four">
+
+                <div className="main-container-four">
+
+                    <div className="wrap-content">
+
+                        <div className="title-four">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         </div>
+
+                        <div className="content-four">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                         </div>
+
+                        <div className="wrap-items">
+                            <div className="left-items"> 
+                            <ul>
+                                <li>
+                                    <span className="item"><i class="fal fa-check"></i></span>
+                                    <span className="item">SEO Services</span>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <span className="item"><i class="fal fa-check"></i>dfgdgdsg</span>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <span className="item">Owen</span>
+                                </li>
+                            </ul>
+                            </div>
+                            <div className="right-items"> </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default SectionFour;
+
+
+
