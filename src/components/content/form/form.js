34771e872d69b3f72a690bd6dea2a7806b89dc3d
Babiy7 <luisowen11@gmail.com>
2018-12-21 18:52:03 +0200

    1) create form 2) form done

---
+++ src/components/content/form/form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../../content/form.css';
+
+class Form extends Component {
+    render() {
+        return (
+            <section className="form">
+
+                <div className="form-container">
+                <h1 className="subscrive">Subscrive</h1>
+                <p className="discription">Receive our updates with offers and plans</p>
+
+                    <form className="subscrive-form">
+                        <input className="search-txt" type="text" placeholder="Enter You Email" />
+                        <input className="search-btn" type="submit" value="subscrive" href="#"/>
+                           
+                        
+                    </form>
+
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Form;
