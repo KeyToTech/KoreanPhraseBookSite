e73dc8c901292a740cfdabee24e3aee4a3de87e8
OlehBabiy <luisowen11@gmail.com>
2019-02-18 12:58:38 +0200

    add image
    add image for table

--- src/container/content/table/table.js
+++ src/container/content/table/table.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import '../table/table.css';
 import '../table/media.css';
-
+import chapters from '../../../image/chapters.png';
+import dictionary from '../../../image/dictionary.png';
+import notes from '../../../image/notes.png';
 
 const wrapContent = [
     {
-        title: "Розділи", discription: "Основні слова та вирази, які допоможуть вам у побутовомуспілкуванні з носіями мови.",
+        image: chapters , title: "Розділи", discription: "Основні слова та вирази, які допоможуть вам у побутовомуспілкуванні з носіями мови.",
         href: "#function-section"
     },
-    { title: "Словник", discription: "Більш ніж 900 слів у алфавітному порядку.", href: "#alphabet-section" },
-    { title: "Нотатки", discription: "Робіть власні нотаткита записуйте все, що хочете запам’ятати.", href: "#search-section" }
+    {
+        image: dictionary ,
+        title: "Словник", discription: "Більш ніж 900 слів у алфавітному порядку.", href: "#alphabet-section"
+    },
+    {
+        image: notes ,
+        title: "Нотатки", discription: "Робіть власні нотаткита записуйте все, що хочете запам’ятати.", href: "#search-section"
+    }
 ]
 var button = "Дізнатися більше";
 
 const table = wrapContent.map((table) =>
     <div className="table">
-        <div className="image"></div>
+        <div className="image-wrap">
+            <img className="image" src={table.image} alt="image" />
+        </div>
         <div className="text-container">
             <h2 className="title">{table.title}</h2>
             <p className="discription">{table.discription}</p>
