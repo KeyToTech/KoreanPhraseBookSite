6e4a78d92bda45e08bb831808df8a91ecfbcaeda
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-02-27 18:05:31 +0200

    add subscription form

--- src/container/message/message.js
+++ src/container/message/message.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './message.css';
+import Form from '../content/form/form';
 
 class Message extends React.PureComponent{
     render(){
-        return<div id="message">
-            <p>іОС додаток вже на фінальній стадії розробки. Підписуйтеся на наші оновлення, щоб першими дізнатися про запуск додатку в Apple Store</p>
-            <a className="MessageButton" href="#" onClick={this.props.showWrap}>Назад</a>
+        return<div>
+            <div id="message">
+                <p>іОС додаток вже на фінальній стадії розробки. Підписуйтеся на наші оновлення, щоб першими дізнатися про запуск додатку в Apple Store</p>
+                <a className="MessageButton" href="#" onClick={this.props.showWrap}>Назад</a>
+            </div>
+            <Form />
         </div>
     }
 }
