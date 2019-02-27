f2088ff37105d39c5672644e8df16a08749200b1
OlehBabiy <luisowen11@gmail.com>
2019-02-27 20:49:16 +0200

    add the screen to the section alphabet

--- src/container/content/alphabet/alphabet.js
+++ src/container/content/alphabet/alphabet.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../alphabet/alphabet.css';
 import '../alphabet/media.css';
-import alphabet from '..//..//../image/image_alphabet.png';
+import alphabet from '..//..//../image/alphabet_framed.png';
+import alphabetPictures from '..//..//../image/image_alphabet.png';
 
 var title = "Для початківців у розмовнику є Корейський алфавіт";
 var discription = " Вважається, що корейський алфавіт можна вивчити всього лише  за 15 хв! Cпробуєте?";
@@ -13,11 +14,13 @@ const Alphabet = () => (
             <p className="discription-alphabet">{discription}</p>
         </div>
         <div className="wrap-screen">
-
             <div className="screen">
-                <img className="image-alphabet" src={alphabet} alt="alphabet" />
+                <img className="screen-img" src={alphabet} alt="alphabet" />
             </div>
+        </div>
 
+        <div className="wrap-picture-alphabet">
+            <img className="alphabet-pictures" src={alphabetPictures} alt="alphabet" />
         </div>
     </div>
 )
