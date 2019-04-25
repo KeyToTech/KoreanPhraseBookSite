1a6284f65fabebca81f6c0b62c304a9b20b096de
OlehBabiy <luisowen11@gmail.com>
2019-04-25 16:37:00 +0300

    Added localization into section Function.

--- src/container/content/functions/functions.js
+++ src/container/content/functions/functions.js
@@ -7,52 +7,15 @@ import middleCircle from '..//../../image/middle_circle.png';
 import smallCircle from '..//../../image/small_circle.png';
 import doubleCircle from '..//../../image/double_circle.png';
 
-var title = "Ми розробили багато цікавих функцій";
-var discription = " Додавайте корисні для вас слова та фрази в улюблене практикуйте реальні побутові ситуації за допомогою діалогів";
-var kTitle = "학습자 위한 유용한 가능";
-var kDiscription = "즐겨찾기에 자주 사용되는 구문 추가 대화를 통해 실제 가정 상황을 연습";
-var item = "";
-var kItem = "";
-const items = ["Улюблене", "Діалоги"];
-const kItems = ["즐겨찾기", "대화"];
-
-function setItem(items, language) {
-    if (language == true) {
-        return (
-            item = items.map((item, index) =>
-                <ul>
-                    <li>
-                        <h2>  <span className="item">{++index}.  {item}</span></h2>
-                    </li>
-                </ul>
-            )
+function setItem(items) {
+    return (
+        items.map((item) =>
+            <li>
+                <h2><span className="item">{item}</span></h2>
+            </li>
         )
-    } else {
-        return (
-            kItem = kItems.map((item, index) =>
-                <ul>
-                    <li>
-                        <h2>  <span className="item">{++index}.  {item}</span></h2>
-                    </li>
-                </ul>
-            )
-        )
-    }
+    )
 }
-
-const changeLanguage = (language) => (
-    language == true
-        ?
-        <div className="text-container-function">
-            <h2 className="title-function">{title}</h2>
-            <p className="discription-function">{discription}</p>
-        </div>
-        :
-        <div className="text-container-function">
-            <h2 className="title-function">{kTitle}</h2>
-            <p className="discription-function">{kDiscription}</p>
-        </div>
-)
 
 const Function = (props) => (
     <div id="function-section">
@@ -66,9 +29,14 @@ const Function = (props) => (
             </div>
         </div>
         <div className="wrap-context-function">
-            {changeLanguage(props.language)}
+            <div className="text-container-function">
+                <h2 className="title-function">{props.language.titleFunctins}</h2>
+                <p className="discription-function">{props.language.descriptionFunctions}</p>
+            </div>
             <div className="container-items">
-                {setItem(items, props.language)}
+                <ul>
+                    {setItem(props.language.itemsFunctions)}
+                </ul>
             </div>
         </div>
     </div>
