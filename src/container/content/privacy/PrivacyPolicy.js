7c854b105c71cbb22b39254d6de0fe305a249fc4
Babiy7 <olegbabiy77@gmail.com>
2019-07-16 14:16:02 +0300

    made similar blocks in one function.

--- src/container/content/privacy/PrivacyPolicy.js
+++ src/container/content/privacy/PrivacyPolicy.js
@@ -1,43 +1,46 @@
 import React from "react";
 import "./privacy.css";
 
-const PrivacyPolicy = () => (
+const containers = [
+  {
+    title: "Collecting personal information",
+    description: "We do not collect any personal information from our users."
+  },
+  {
+    title: "Third-party disclosure",
+    description:
+      "We do not sell, trade, or otherwise transfer to outside parties any information of the user."
+  },
+  {
+    title: "Third-party links",
+    description:
+      "We do not include third-party products or services in our application."
+  },
+  {
+    title: "Changes to This Privacy Policy",
+    description: "This policy may be updated from time to time."
+  },
+  {
+    title: "Contact Us",
+    description:
+      "If there are any questions regarding this privacy policy, please send us an e-mail at info@keytotech.com"
+  }
+];
+
+const container = () =>
+  containers.map(({ title, description }) => {
+    return (
+      <div className="container">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    );
+  });
+
+const PrivacyPolicy = props => (
   <div className="privacy-content">
     <h1>Privacy Policy</h1>
-    <div className="main-container">
-      <div className="container">
-        <h2>Collecting personal information</h2>
-        <p>We do not collect any personal information from our users.</p>
-      </div>
-
-      <div className="container">
-        <h2>Third-party disclosure</h2>
-        <p>
-          We do not sell, trade, or otherwise transfer to outside parties any
-          information of the user.
-        </p>
-      </div>
-
-      <div className="container">
-        <h2>Third-party links</h2>
-        <p>
-          We do not include third-party products or services in our application.
-        </p>
-      </div>
-
-      <div className="container">
-        <h2>Changes to This Privacy Policy</h2>
-        <p>This policy may be updated from time to time.</p>
-      </div>
-
-      <div className="container">
-        <h2>Contact Us</h2>
-        <p>
-          If there are any questions regarding this privacy policy, please send
-          us an e-mail at info@keytotech.com
-        </p>
-      </div>
-    </div>
+    <div className="main-container">{container}</div>
   </div>
 );
 
