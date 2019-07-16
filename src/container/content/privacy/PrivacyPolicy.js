d787c6e1619eb6f1cbfe50389b755b3b27688f6a
Babiy7 <olegbabiy77@gmail.com>
2019-07-16 14:04:29 +0300

    added new conteiners.

--- src/container/content/privacy/PrivacyPolicy.js
+++ src/container/content/privacy/PrivacyPolicy.js
@@ -4,29 +4,40 @@ import "./privacy.css";
 const PrivacyPolicy = () => (
   <div className="privacy-content">
     <h1>Privacy Policy</h1>
+    <div className="main-container">
+      <div className="container">
+        <h2>Collecting personal information</h2>
+        <p>We do not collect any personal information from our users.</p>
+      </div>
 
-    <h2>Collecting personal information</h2>
-    <p>We do not collect any personal information from our users.</p>
+      <div className="container">
+        <h2>Third-party disclosure</h2>
+        <p>
+          We do not sell, trade, or otherwise transfer to outside parties any
+          information of the user.
+        </p>
+      </div>
 
-    <h2>Third-party disclosure</h2>
-    <p>
-      We do not sell, trade, or otherwise transfer to outside parties any
-      information of the user.
-    </p>
+      <div className="container">
+        <h2>Third-party links</h2>
+        <p>
+          We do not include third-party products or services in our application.
+        </p>
+      </div>
 
-    <h2>Third-party links</h2>
-    <p>
-      We do not include third-party products or services in our application.
-    </p>
+      <div className="container">
+        <h2>Changes to This Privacy Policy</h2>
+        <p>This policy may be updated from time to time.</p>
+      </div>
 
-    <h2>Changes to This Privacy Policy</h2>
-    <p>This policy may be updated from time to time.</p>
-
-    <h2>Contact Us</h2>
-    <p>
-      If there are any questions regarding this privacy policy, please send us
-      an e-mail at info@keytotech.com
-    </p>
+      <div className="container">
+        <h2>Contact Us</h2>
+        <p>
+          If there are any questions regarding this privacy policy, please send
+          us an e-mail at info@keytotech.com
+        </p>
+      </div>
+    </div>
   </div>
 );
 
