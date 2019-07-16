fa6ab8e4e46d094cebd19f7c5caec849bb532dad
Babiy7 <olegbabiy77@gmail.com>
2019-07-16 13:46:28 +0300

    added both margin-top and margin-bottom for the PrivacyPolicy component.

--- src/container/content/wrap.js
+++ src/container/content/wrap.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import '../content/wrap.css';
+import React, { Component } from "react";
+import "./wrap.css";
 import Table from "../content/table/table.js";
 import Functions from "../content/functions/functions.js";
 import Alphabet from "../content/alphabet/alphabet.js";
@@ -7,17 +7,17 @@ import Search from "../content/search/search.js";
 import Form from "../content/form/form.js";
 
 class Wrap extends Component {
-    render() {
-        return (
-            <div className="wrap-content">
-                <Table language={this.props.language} />
-                <Functions language={this.props.language} />
-                <Alphabet language={this.props.language} />
-                <Search language={this.props.language} />
-                <Form language={this.props.language} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="wrap-content">
+        <Table language={this.props.language} />
+        <Functions language={this.props.language} />
+        <Alphabet language={this.props.language} />
+        <Search language={this.props.language} />
+        <Form language={this.props.language} />
+      </div>
+    );
+  }
 }
 
 export default Wrap;
